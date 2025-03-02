caaff47d216e4606db605418625704b70165c8f7
Bill Nagbe <BillyNagbe@gmail.com>
2025-03-02 13:06:42 -0600

    cards setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-
+import Cards from "./cards";
 
 export default function Home() {
   return (
     <div className="min-h-screen min-w-2/5 max-w-4/6 border-4 rounded-xl px-4 py-8">
       <div className="grid grid-cols-6 grid-rows-9 gap-4 ">
-        
+        <Cards />
+        <Cards />
+        <Cards />
       </div>
     </div>
   );
