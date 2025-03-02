750d6660bc88c68d2df181bd6ab06f9f98074961
Bill Nagbe <BillyNagbe@gmail.com>
2025-03-02 13:39:09 -0600

    update card layout and info

--- app/cards.tsx
+++ app/cards.tsx
@@ -1,8 +1,27 @@
+import Image from "next/image"
+
+
 
 export default function Cards({props}: any) {
         return (
-            <div className="w-[250px] h-[500px] border rounded-xl shadow-md shadow-black">
-                
-            </div>
+            <figure className="w-[250px] h-[500px] border rounded-xl shadow-md shadow-black grid grid-cols-4 grid-rows-4">
+                <Image
+                src={props.src}
+                alt={props.alt}
+                height={props.height}
+                width={props.width}
+                className="row-start-1 row-end-4 col-start-1 col-end-4"
+                />
+                <div className="col-start-3 col-end-5">
+                    <ul className="list-none p-1.5 flex flex-col justify-center items-center">
+                        <li>PPG: {props.ppg}</li>
+                        <li>RPG: {props.rpg}</li>
+                        <li>AST: {props.ast}</li>
+                    </ul>
+                </div>
+                <figcaption className="row-start-3">
+                    `${props.firstName} ${props.lastName}, `
+                </figcaption>
+            </figure>
         )
 } 
