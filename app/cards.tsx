7383bf63623be09e0b2a7fb75b806ef742e788df
Bill Nagbe <BillyNagbe@gmail.com>
2025-03-02 13:03:25 -0600

    wolves collection

---
+++ app/cards.tsx
@@ -0,0 +1,8 @@
+
+export default function Cards({props}: any) {
+        return (
+            <div className="w-[250px] h-[500px] border rounded-xl shadow-md shadow-black">
+                
+            </div>
+        )
+} 
